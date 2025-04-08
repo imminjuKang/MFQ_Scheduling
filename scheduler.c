d9952e7545d778cc78f7632e5118290624d39b74
강민주 <162291384+imminjuKang@users.noreply.github.com>
2025-04-08 19:54:44 +0900

    Create scheduler.c

---
+++ scheduler.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void q0()
+{
+    int* pqueue0;
+
+
+}
+
+void q1()
+{
+    int* pqueue1;
+
+
+}
+
+void q2()
+{
+    int* pqueue2;
+
+}
+
+int main()
+{
+    FILE *fp;
+
+}
