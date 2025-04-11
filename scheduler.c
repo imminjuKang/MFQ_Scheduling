cbaa7300e4dd4f57ec3afbcaa9a21850ef13b8f4
강민주 <162291384+imminjuKang@users.noreply.github.com>
2025-04-11 11:12:33 +0900

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define TIME_QUANTUM0 2
 #define TIME_QUANTUM1 4
-#define MAX_LENGTH 1000
+#define MAX_LENGTH 100
 
 typedef struct Process
 {
@@ -11,112 +11,62 @@ typedef struct Process
     int remain_time;
     int finish_time;
     int current_q;
-    int used_time;
 
 } Process;
 
-void queue0(Process** q0, int* q0_size, Process** q1, int* q1_size, int* clock_time, int* gantt_chart)
+typedef struct Queue
 {
-    if (q0[0]->used_time < TIME_QUANTUM0 && q0[0]->remain_time > 0)
-    {
-        gantt_chart[*clock_time] = q0[0]->pid;
-        q0[0]->remain_time--;
-        (q0[0]->used_time)++;
-        return;
-    }
+    int front;
+    int rear;
+    int size;
+    Process* process_info[MAX_LENGTH];
 
-    // time slice 끝 혹은 remain_time <= 0인 경우
-    else
-    {
-        // cpu 더 이상 필요 없이 끝났기 때문에 finish
-        if (q0[0]->remain_time == 0)
-        {
-            q0[0]->finish_time = *clock_time;
-        }
-        // burst time이 남아 있기 때문에 q1로 이동
-        else
-        {
-            q0[0]->current_q = 1;
-            q0[0]->used_time = 0;
-            q1[*q1_size++] = q0[0];
-        }
+} Queue;
 
-        // 원활한 관리를 위해 한 칸씩 앞으로 옮기기
-        for (int i = 1; i < *q0_size; i++)
-        {
-            q0[i - 1] = q0[i];
-        }
-
-        (*q0_size)--;
-        return;
-    }
+void enqueue(Queue* q, Process* p)
+{
+    q->process_info[q->rear] = p;
+    q->rear++;
+    q->size++;
 }
 
-void queue1(Process** q1, int* q1_size, Process** q2, int* q2_size, int* clock_time, int* gantt_chart)
+Process* dequeue(Queue* q)
 {
-    if (q1[0]->used_time < TIME_QUANTUM1 && q1[0]->remain_time > 0)
-    {
-        gantt_chart[*clock_time] = q1[0]->pid;
-        q1[0]->remain_time--;
-        (q1[0]->used_time)++;
-        return;
-    }
-
-    else
-    {
-        if (q1[0]->remain_time == 0)
-        {
-            q1[0]->finish_time = *clock_time;
-        }
-        else
-        {
-            q1[0]->current_q = 2;
-            q1[0]->used_time = 0;
-            q2[*q2_size++] = q1[0];
-        }
-
-        for (int i = 1; i < *q1_size; i++)
-        {
-            q1[i - 1] = q1[i];
-        }
-        (*q1_size)--;
-        return;
-    }
+    Process* p = q->process_info[q->front];
+    q->front++;
+    q->size--;
+    return p;
 }
 
-void queue2(Process** q2, int* q2_size, int* clock_time, int* gantt_chart)
+int isEmpty(Queue* q)
 {
-    Process* temp;
-    int time = 0;
+    if (q->front == q->rear) return 1;
+    else return 0;
+}
 
-    //편의를 위해 q2 함수를 시작할 때는 항상 정렬부터 해서
-    //remain time이 가장 짧게 남은 process가 가장 앞에 오게 한다
-    for (int i = 0; i < *q2_size; i++)
+Process* spn(Queue* q)
+{
+    if (q->size == 0) return NULL;
+
+    int min = q->front;
+    for (int i = q->front + 1; i < q->rear; i++) 
     {
-        for (int j = 0; j < (*q2_size -1) - i; j++)
+        if (q->process_info[i]->remain_time < q->process_info[min]->remain_time) 
         {
-            if (q2[j]->remain_time > q2[j+1]->remain_time)
-            {
-                temp = q2[j];
-                q2[j] = q2[j+1];
-                q2[j+1] = temp;
-            }
+            min = i;
         }
     }
 
-    if (q2[0]->remain_time > 0)
-    {
-        gantt_chart[*clock_time] = q2[0]->pid;
-        q2[0]->remain_time--;
-        (q2[0]->used_time)++;
-        return;
+    Process* shortest = q->process_info[min];
+
+    for (int i = min; i < q->rear - 1; i++) {
+        q->process_info[i] = q->process_info[i + 1];
     }
-    //remain time 끝나면 finish time 기록하고 return
-    else if (q2[0]->remain_time == 0)
-    {
-        q2[0]->finish_time = *clock_time;
-        return;
-    }
+
+    q->rear--;
+    q->size--;
+
+    return shortest;
 }
 
 int main()
@@ -140,10 +90,6 @@ int main()
 
     // 프로세스를 담고 있는 배열
     Process process[num_process];
-    // 프로세스 배열에 있는 프로세스 정보를 직접 관리하기 위해 포인터로 선언
-    Process* q0[num_process];
-    Process* q1[num_process];
-    Process* q2[num_process];
 
     for (int i = 0; i < num_process; i++)
     {
@@ -156,62 +102,81 @@ int main()
     }
 
     fclose(fp);
-    
-    // 남은 시간을 burst_time으로 초기화하기
-    for (int i = 0; i < num_process; i++)
-    {
-        process[i].remain_time = process[i].burst_time;
-        process[i].finish_time = 0;
-    }
 
-    // main 함수에서는 cpu를 구현
+    Queue q0, q1, q2;
+    q0.front = q0.rear = q0.size = 0;
+    q1.front = q1.rear = q1.size = 0;
+    q2.front = q2.rear = q2.size = 0;
+
+    int gantt_chart[MAX_LENGTH] = {0};
     int clock_time = 0;
-    int gantt_chart[MAX_LENGTH];
 
-    // queue size 관리
-    int q0_size = 0;
-    int q1_size = 0;
-    int q2_size = 0;
+    Process *running_p = NULL;
+    int time_quantum = 0;
 
-    // 원활한 관리를 위해 1초 단위로 관리
     while (1)
     {
-        // cpu clock time과 arrival time이 같으면 q0으로 들어감
         for (int i = 0; i < num_process; i++)
         {
             if (process[i].arrival_time == clock_time)
             {
-                process[i].current_q = 0;
-                process[i].used_time = 0;
-                q0[q0_size++] = &process[i];
+                enqueue(&q0, &process[i]);
             }
         }
 
-        // q0부터 q1, q2 순서로 실행
-        // q0이 비어 있지 않으면, q0부터 cpu 할당
-        if (q0_size > 0)
+        if (running_p != NULL)
         {
-            queue0(q0, &q0_size, q1, &q1_size, &clock_time, gantt_chart);
+            gantt_chart[clock_time] = running_p->pid;
+            running_p->remain_time--;
+            time_quantum++;
+            if (running_p->remain_time == 0)
+            {
+                running_p->finish_time = clock_time;
+                running_p = NULL;
+                time_quantum = 0;
+            }
+            else if (running_p->current_q == 0 && time_quantum == TIME_QUANTUM0)
+            {
+                enqueue(&q1, running_p);
+                running_p = NULL;
+                time_quantum = 0;
+            }
+            else if (running_p->current_q == 1 && time_quantum == TIME_QUANTUM1)
+            {
+                enqueue(&q2, running_p);
+                running_p = NULL;
+                time_quantum = 0;
+            }
         }
-        // q0이 비어 있으면 q1 확인 후 cpu 할당
-        else if (q1_size > 0)
+
+        if (running_p == NULL)
         {
-            queue1(q1, &q1_size, q2, &q2_size, &clock_time, gantt_chart);
-        }
-        // q1도 비어 있으면 q2 확인 후 cpu 할당
-        else if (q2_size > 0)
-        {
-            queue2(q2, &q2_size, &clock_time, gantt_chart);
-        }
-        // 세 개의 큐가 모두 비어 있으면 cpu clock time은 끝남(while loop 탈출)
-        else
-        {
-            break;
+            if (!isEmpty(&q0))
+            {
+                running_p = dequeue(&q0);
+                running_p->current_q = 0;
+                time_quantum = 0;
+            }
+            else if (!isEmpty(&q1))
+            {
+                running_p = dequeue(&q1);
+                running_p->current_q = 1;
+                time_quantum = 0;
+            }
+            else if (!isEmpty(&q2))
+            {
+                running_p = spn(&q2);  
+                running_p->current_q = 2;
+                time_quantum = 0;
+            }
+            else
+            {
+                break;
+            }
         }
 
         clock_time++;
     }
-
 
     // 출력
     // Gantt Chart
@@ -257,6 +222,10 @@ int main()
         printf("%3d %5d %6d\n", process[i].pid, turnaround_time, waiting_time);
     }
 
+    // 평균 TT, WT
+    printf("\nAvergae TT: %.1f\n", avg_tt / num_process);
+    printf("Average WT: %.1f", avg_wt / num_process);
+}
     // 평균 TT, WT
     printf("\nAvergae TT: %.1f\n", avg_tt / num_process);
     printf("Average WT: %.1f", avg_wt / num_process);
